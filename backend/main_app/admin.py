f34d141d80cdb9fb31479e0cdc1257ccf380e089
ideens <ideens@users.noreply.github.com>
2021-12-03 01:59:34 +0000

    added order models and registered

--- backend/main_app/admin.py
+++ backend/main_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review, Order, OrderItem, OrderDelivery
 
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(OrderDelivery)
