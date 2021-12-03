f520385a1ef27a3743f2d5a756289060507da9ab
ideens <ideens@users.noreply.github.com>
2021-12-03 02:12:09 +0000

    pillow installed

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100, null=True, blank=True)
-    # image = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField(max_length=300, null=True, blank=True)
     category = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
