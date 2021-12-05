fba3ec1e1dbaba9e1498b545f75643f2e09b2138
ideens <ideens@users.noreply.github.com>
2021-12-05 17:23:56 +0000

    getting data with reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,11 +9,11 @@ import {
 // Actions are plain JavaScript objects that have a type field.
 // An event that describes something that happened in the application.
 
-export const productsList = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
     //this swill set off respective reducer, loading = true, empty array of products
-    const { data } = await axios.get('api/products/')
+    const { data } = await axios.get('/api/products/')
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     // has now set state to data
   } catch (error) {
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,19 +3,19 @@ import { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product.js'
 import { useDispatch, useSelector } from 'react-redux'
-import { productsList } from '../actions/productActions'
+import { listProducts } from '../actions/productActions'
 
 const Home = () => {
   const dispatch = useDispatch()
   const productList = useSelector((state) => state.productList)
-  // const { error, loading, products } = productList
-  const products = []
+  // using useSelector to tell it what part of the state we want to get
+  const { error, loading, products } = productList
 
   useEffect(() => {
     console.log('use effect reached')
-    dispatch(productsList())
+    dispatch(listProducts())
     // updates store
-  }, [])
+  }, [dispatch])
 
   // const [products, setProducts] = useState([])
 
@@ -30,13 +30,19 @@ const Home = () => {
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   )
 }
