4b6fdb82647231d0bdbb2b0aa7526ef4331c4876
ideens <ideens@users.noreply.github.com>
2021-12-06 21:27:35 +0000

    followed authentication install steps, jwt

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import ProductsDetailView, ProductsListView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 
 urlpatterns = [
+    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("products/", ProductsListView.as_view()),
     path("products/<int:pk>/", ProductsDetailView.as_view()),
 ]
